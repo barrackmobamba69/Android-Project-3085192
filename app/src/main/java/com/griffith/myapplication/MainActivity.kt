5d542162b2f674eeab296a744a7415b54071c84c
Udayy Singh Pawar <106630515+barrackmobamba69@users.noreply.github.com>
2024-11-13 15:19:38 +0000

    Added a theme

--- app/src/main/java/com/griffith/myapplication/MainActivity.kt
+++ app/src/main/java/com/griffith/myapplication/MainActivity.kt
@@ -26,13 +26,16 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.compose.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NavigationScreen()
+            AppTheme(darkTheme = true){
+                NavigationScreen()
+            }
         }
     }
 }
